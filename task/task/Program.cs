e9ed6edbc13bedbd4b6eeb44af81aa54b53b0bb3
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-05-28 12:37:50 +0300

    Add files via upload

---
+++ task/task/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+    class Program
+    {
+        enum Actions { OutPutAllStudents = 1,OutPutMaleStudents, OutPutFemaleStudents,InPut,Exit}
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Clear();
+                List<Student> st = new List<Student>();
+                Console.Write("1 - Вывести список всех студентов.\n2 - Вывести список всех студентов-юношей.\n3 - Вывести список всех студентов-девушек.\n4 - Добавить студента в список.\n5 - Exit.\n\n");
+                int act = int.Parse(Console.ReadLine());
+                switch(act)
+                {
+                    case 1:
+                        st.Add(Student.Input());
+                        break;
+                    case 2:
+                        Student.OutPutAll(st);
+                        break;
+                }
+                //switch((Actions)act)
+                //{
+                //    case Actions.OutPutAllStudents:
+                //        Console.Clear();
+                //        stud.OutPutAll();
+                        
+                //        break;
+                //    //case Actions.OutPutMaleStudents:
+                //    //    Console.Clear();
+                //    //    foreach (var student in st)
+                //    //    {
+                //    //        if(student.Gend == Gender.Male)
+                //    //        {
+                //    //            student.OutPut();
+                //    //        }    
+                //    //    }
+                //    //    Console.Write("Нажмите любую клавишу, чтобы вернуться назад.");
+                //    //    char message2 = Console.ReadKey().KeyChar;
+                //    //    break;
+                //    //case Actions.OutPutFemaleStudents:
+                //    //    Console.Clear();
+                //    //    foreach (var student in st)
+                //    //    {
+                //    //        if (student.Gend == Gender.Female)
+                //    //        {
+                //    //            student.OutPut();
+                //    //        }
+                //    //    }
+                //    //    Console.Write("Нажмите любую клавишу, чтобы вернуться назад.");
+                //    //    char message3 = Console.ReadKey().KeyChar;
+                //    //    break;
+                //    case Actions.InPut:
+                //        Console.Clear();
+                //        stud.Input();
+                //        break;
+                //    case Actions.Exit:
+                //        return;
+                //}
+            }    
+            
+        }
+
+        
+    }
+}
---
+++ task/task/Student.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+    public enum Gender { Male, Female }
+    class Student
+    {
+        public string Fio { get; set; }
+        public int Date { get; set; }
+        //public Gender Gend {get;set;}
+        public Student() { }
+        public Student(string fio, int date)
+        {
+            //try
+            //{
+                this.Fio = fio;
+                this.Date = date;
+                //this.Gend = (Gender)Enum.Parse(typeof(Gender) , gend);
+            //}
+            //catch
+            //{
+            //    Console.Clear();
+            //    Console.Write("Данные введены неправильно.\nНажмите любую клавишу, чтобы вернуться назад.\n");
+            //    char message = Console.ReadKey().KeyChar;
+            //}
+            
+        }
+        public static Student Input()
+        {
+            //try
+            //{
+                Console.Write($"Введите ФИО студента: ");
+                string fio = Console.ReadLine();
+                Console.Write($"Введите год рождения студента: ");
+                int date = int.Parse(Console.ReadLine());
+                //Console.Write($"Введите гендер студента: ");
+                //string gend = Console.ReadLine();
+                return new Student(fio, date);
+                //Console.Write("Нажмите любую клавишу, чтобы вернуться назад.");
+                //char message4 = Console.ReadKey().KeyChar;
+
+
+
+        }
+        public void OutPut()
+        {
+            Console.Write($"{Fio}\n{Date}");
+        }
+        public static void OutPutAll(List<Student> students)
+        {
+            foreach(var st in students)
+            {
+                st.OutPut();
+            }
+            Console.Write("Нажмите любую клавишу, чтобы вернуться назад.");
+            char message4 = Console.ReadKey().KeyChar;
+        }
+    }
+}
